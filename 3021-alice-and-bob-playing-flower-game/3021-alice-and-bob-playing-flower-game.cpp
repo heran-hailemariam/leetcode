0cd7a1bc31a6e52f27456143c9843006f5967e1c
Heran Hailemariam Kahsay <heranhailemariam@gmail.com>
2026-01-25 13:02:17 -0400

    Time: 0 ms (100%), Space: 8.6 MB (12.93%) - LeetHub

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-
-        long long ans = 1LL*n*m/2;
-        return ans;
-        
+        return((((m%2 != 0)+(m/2)) * long(n/2)) + (((n%2!=0) +n/2) * long(m/2)));
     }
 };
