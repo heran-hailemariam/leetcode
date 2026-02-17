4a567395c848a96486f1611113a329f44f452cf3
Heran Hailemariam Kahsay <heranhailemariam@gmail.com>
2026-02-17 15:33:36 -0400

    Time: 3 ms (9.95%), Space: 10 MB (24.16%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    vector<string> readBinaryWatch(int turnedOn) {
-        vector<string> ans;
-        
-        for(int h=0;h<12;h++){
-            for(int m=0;m<60;m++){
-                if(__builtin_popcount(h) + __builtin_popcount(m) == turnedOn){
-                    string time = to_string(h) + ":";
-                    if (m < 10) time += "0";
-                    time += to_string(m);
-                    ans.push_back(time);
-                }
-            }
+    vector<string> readBinaryWatch(int k) {
+        if (k == 0) return {"0:00"};
+        if (k > 8) return {};
+        vector<string> res;
+        int q = (1 << k) - 1;
+
+        while (q < (1 << 10)) {
+            string time = isValid(q);
+            if (!time.empty()) res.push_back(time);
+            int r = q & -q;
+            int n = q + r;
+            //Find the Next Bit Combination (with the same hamming weight)
+            //0000000111 -> 0000001011 -> 0000001101 -> 0000001110 -> 0000010011
+            //0000010101 -> 0000010110 -> 0000011001 -> 0000011010 -> 0000011100 
+            q = (((n ^ q) >> 2) / r) | n;
         }
-        return ans;
+        return res;
+    }
+
+    string isValid(int q) {
+        int hour = q >> 6;
+        int min = q & 63;
+        if (hour >= 12 || min >= 60) return "";
+        //return format("{}:{:02}", hour, min);
+        return to_string(hour) + ":" + (min < 10 ? "0" : "") + to_string(min);
     }
 };
